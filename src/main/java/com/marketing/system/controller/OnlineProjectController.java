f0767a5bc5e2311a36ac98f8fb37ce359762fa0e
weifei <912118614@qq.com>
2017-09-26 09:56:37 +0800

    -----

---
+++ src/main/java/com/marketing/system/controller/OnlineProjectController.java
@@ -0,0 +1,26 @@
+package com.marketing.system.controller;
+
+
+@Api(description = "上线待审批接口", value = "上线待审批接口")
+@Scope("prototype")
+@RestController
+//@Controller
+@EnableAutoConfiguration
+public class UpProjectController {
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+
+
+    /**
+     * 查询立项待审批列表
+     *
+     * @return
+     */
+    @ApiOperation(value = "查询上线待审批列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "current", value = "当前页", required = true, dataType = "Integer"),
+            @ApiImplicitParam(paramType = "query", name = "pageSize", value = "每页显示条数", required = true, dataType = "Integer"),
+            @ApiImplicitParam(paramType = "query", name = "department", value = "项目发起部门", required = false, dataType = "String")
+    })
+
+}

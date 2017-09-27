1a3f21cadaeb7569c73b5c35db600b3b96245ba0
wangdong <447141101@qq.com>
2017-09-27 09:27:27 +0800

    增加首页接口

--- src/main/java/com/marketing/system/service/impl/IndexServerImpl.java
+++ src/main/java/com/marketing/system/service/impl/IndexServerImpl.java
@@ -2,7 +2,6 @@ package com.marketing.system.service.impl;
 
 import com.marketing.system.entity.ProjectInfo;
 import com.marketing.system.entity.ProjectSubtask;
-import com.marketing.system.entity.ProjectTask;
 import com.marketing.system.mapper_two.ProjectInfoMapper;
 import com.marketing.system.mapper_two.ProjectSubtaskMapper;
 import com.marketing.system.mapper_two.ProjectTaskMapper;
@@ -79,10 +78,10 @@ public class IndexServerImpl implements IndexService {
 
         //循环计算距逾期时间天数
         for (int i = 0; i < infoList.size(); i++) {
-            for (ProjectInfo model : infoList) {
+            for (ProjectInfo projectInfo : infoList) {
 
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date plansdate = sdf.parse(model.getPlansdate());//预计完成时间
+                Date plansdate = sdf.parse(projectInfo.getPlansdate());//预计完成时间
 
                 Calendar calendar = new GregorianCalendar();
                 calendar.setTime(plansdate);
@@ -97,11 +96,11 @@ public class IndexServerImpl implements IndexService {
                 long time2 = cal.getTimeInMillis();
                 String betweenDays = ((time2 - time1) / (1000 * 3600 * 24))+"";
 
-                model.setBetweenDays(Integer.valueOf(betweenDays));
+                projectInfo.setBetweenDays(Integer.valueOf(betweenDays));
 
                 //项目距离完成只有6天时间时即在首页提示
-                if (model.getBetweenDays() < 6 || model.getBetweenDays() == 6) {
-                    infoList.set(i,model);
+                if (projectInfo.getBetweenDays() < 6 || projectInfo.getBetweenDays() == 6) {
+                    infoList.set(i,projectInfo);
                 }
             }
 
@@ -122,10 +121,10 @@ public class IndexServerImpl implements IndexService {
         }
         //循环计算距逾期时间天数
         for (int i = 0; i < infoList.size(); i++) {
-            for (Map model : infoList) {
+            for (Map map : infoList) {
 
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date edate = sdf.parse(String.valueOf(model.get("edate")));//预计完成时间
+                Date edate = sdf.parse(String.valueOf(map.get("edate")));//预计完成时间
 
                 Calendar calendar = new GregorianCalendar();
                 calendar.setTime(edate);
@@ -140,11 +139,11 @@ public class IndexServerImpl implements IndexService {
                 long time2 = cal.getTimeInMillis();
                 long betweenDays = (time2 - time1) / (1000 * 3600 * 24);
 
-                model.put("betweenDays",betweenDays);
+                map.put("betweenDays",betweenDays);
 
                 //任务距离完成时间只有3天时间师即在首页提示
-                if (Integer.valueOf(String.valueOf(model.get("betweenDays"))) < 3 || Integer.valueOf(String.valueOf(model.get("betweenDays"))) == 3) {
-                    infoList.set(i,model);
+                if (Integer.valueOf(String.valueOf(map.get("betweenDays"))) < 3 || Integer.valueOf(String.valueOf(map.get("betweenDays"))) == 3) {
+                    infoList.set(i,map);
                 }
             }
 
@@ -166,10 +165,10 @@ public class IndexServerImpl implements IndexService {
 
         //循环计算距逾期时间天数
         for (int i = 0; i < infoList.size(); i++) {
-            for (ProjectSubtask model : infoList) {
+            for (ProjectSubtask projectSubtask : infoList) {
 
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date edate = sdf.parse(model.getEdate());//预计完成时间
+                Date edate = sdf.parse(projectSubtask.getEdate());//预计完成时间
 
                 Calendar calendar = new GregorianCalendar();
                 calendar.setTime(edate);
@@ -184,11 +183,11 @@ public class IndexServerImpl implements IndexService {
                 long time2 = cal.getTimeInMillis();
                 long betweenHours = (time2 - time1) / (1000 * 3600);
 
-                model.setBetweenHours(betweenHours);
+                projectSubtask.setBetweenHours(betweenHours);
 
                 //子任务距离逾期时间只有1天时间时即在首页提示
-                if (model.getBetweenHours() < 24 || model.getBetweenHours() == 24) {
-                    infoList.set(i,model);
+                if (projectSubtask.getBetweenHours() < 24 || projectSubtask.getBetweenHours() == 24) {
+                    infoList.set(i,projectSubtask);
                 }
             }
 

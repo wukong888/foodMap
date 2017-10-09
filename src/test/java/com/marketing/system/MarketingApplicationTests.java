40dfd2c497b201a8284f84dd811760b866a95cee
wangdong <447141101@qq.com>
2017-10-09 16:10:53 +0800

    去掉测试类

--- src/test/java/com/marketing/system/MarketingApplicationTests.java
+++ src/test/java/com/marketing/system/MarketingApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MarketingApplicationTests {
 
-	@Test
+	/*@Test
 	public void contextLoads() {
 	}
-
+*/
 }

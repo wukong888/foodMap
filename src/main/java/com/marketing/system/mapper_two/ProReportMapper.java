e360e68ead39c231d07b5e295209594b6f22adba
weifei <912118614@qq.com>
2017-10-17 16:08:18 +0800

    -----

--- src/main/java/com/marketing/system/mapper_two/ProReportMapper.java
+++ src/main/java/com/marketing/system/mapper_two/ProReportMapper.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public interface ProReportMapper {
 
     //查询产品类型的项目统计 createsquad,creater,createdate1,createdate2,finishdate1,finishdate2,plansdate1,plansdate2,protype,param
-    @Select("select squadId as squadd,count(*) as sum1 from project_task join project_info on project_task.proId=project_info.proId where project_info.proType=1 and project_info.createrSquadId " +
+    @Select("select squadId as squadd,count(*) as sum1 from project_task join project_info on project_task.proId=project_info.proId where  project_info.proState=4 and project_info.proType=1 and project_info.createrSquadId " +
             " like '%${creatersquadid}%' and project_info.creater like '%${creater}%' and project_info.createDate >= #{createdate1} and project_info.createDate <= #{createdate2} and project_info.finishDate >=#{finishdate1} and project_info.finishDate <= #{finishdate2} and " +
             " project_info.onlineDate >= #{onlinedate1} and project_info.onlineDate <=#{onlinedate2}  and project_info.proName like '%${param}%' GROUP BY project_task.squadId ")
     List<Map> selectType1Sum(@Param("creatersquadid")String creatersquadid,@Param("creater")String creater,@Param("createdate1")String createdate1,@Param("createdate2")String createdate2
@@ -17,7 +17,7 @@ public interface ProReportMapper {
 
 
     //查询活动类型的项目统计 createsquad,creater,createdate1,createdate2,finishdate1,finishdate2,plansdate1,plansdate2,protype,param
-    @Select("select squadId as squadd,count(*) as sum2 from project_task join project_info on project_task.proId=project_info.proId where project_info.proType=2 and project_info.createrSquadId " +
+    @Select("select squadId as squadd,count(*) as sum2 from project_task join project_info on project_task.proId=project_info.proId where project_info.proState=4 and project_info.proType=2 and project_info.createrSquadId " +
             " like '%${creatersquadid}%' and project_info.creater like '%${creater}%' and project_info.createDate >= #{createdate1} and project_info.createDate <= #{createdate2} and project_info.finishDate >=#{finishdate1} and project_info.finishDate <= #{finishdate2} and " +
             " project_info.onlineDate >= #{onlinedate1} and project_info.onlineDate <=#{onlinedate1}  and project_info.proName like '%${param}%' GROUP BY project_task.squadId ")
     List<Map> selectType2Sum(@Param("creatersquadid")String creatersquadid,@Param("creater")String creater,@Param("createdate1")String createdate1,@Param("createdate2")String createdate2

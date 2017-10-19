3504b4cad4828937d4615f5f8e1cccaafe9540fc
wangdong <447141101@qq.com>
2017-10-19 11:20:06 +0800

    测试修改

--- src/main/java/com/marketing/system/util/ToolUtil.java
+++ src/main/java/com/marketing/system/util/ToolUtil.java
@@ -6,46 +6,46 @@ import org.apache.http.HttpResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ToolUtil {
     //文件响应下载
-    public static void downloadFile(File file, OutputStream out){
+    public static void downloadFile(File file, OutputStream out) {
         try {
-        FileInputStream ins = new FileInputStream(file);
-        System.out.println("ins--------"+ins);
-        byte[] b = new byte[1024];
-        int n=0;
-        while((n=ins.read(b))!=-1){
-            out.write(b, 0, n);
-            System.out.println("b---------"+b);
-        }
+            FileInputStream ins = new FileInputStream(file);
+            System.out.println("ins--------" + ins);
+            byte[] b = new byte[1024];
+            int n = 0;
+            while ((n = ins.read(b)) != -1) {
+                out.write(b, 0, n);
+                System.out.println("b---------" + b);
+            }
 
-        ins.close();
-        out.close();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+            ins.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //集合分页
-    public static List<Map> listSplit(int page, int limit, List<Map> list){
+    public static List<Map> listSplit(int page, int limit, List<Map> list) {
 
         List<Map> result = new ArrayList<Map>();
-        if(list != null && list.size() > 0){
+        if (list != null && list.size() > 0) {
             int allCount = list.size();
-            int pageCount = (allCount + limit-1) / limit;
-            if(page >= pageCount){
+            int pageCount = (allCount + limit - 1) / limit;
+            if (page >= pageCount) {
                 page = pageCount;
             }
-            int start = (page-1) * limit;
+            int start = (page - 1) * limit;
             int end = page * limit;
-            if(end >= allCount){
+            if (end >= allCount) {
                 end = allCount;
             }
-            for(int i = start; i < end; i ++){
+            for (int i = start; i < end; i++) {
                 result.add(list.get(i));
             }
         }
@@ -55,21 +55,21 @@ public class ToolUtil {
     }
 
     //集合分页
-    public static List<ProjectInfo> listSplit2(int page, int limit, List<ProjectInfo> list){
+    public static List<ProjectInfo> listSplit2(int page, int limit, List<ProjectInfo> list) {
 
         List<ProjectInfo> result = new ArrayList<ProjectInfo>();
-        if(list != null && list.size() > 0){
+        if (list != null && list.size() > 0) {
             int allCount = list.size();
-            int pageCount = (allCount + limit-1) / limit;
-            if(page >= pageCount){
+            int pageCount = (allCount + limit - 1) / limit;
+            if (page >= pageCount) {
                 page = pageCount;
             }
-            int start = (page-1) * limit;
+            int start = (page - 1) * limit;
             int end = page * limit;
-            if(end >= allCount){
+            if (end >= allCount) {
                 end = allCount;
             }
-            for(int i = start; i < end; i ++){
+            for (int i = start; i < end; i++) {
                 result.add(list.get(i));
             }
         }
@@ -78,4 +78,33 @@ public class ToolUtil {
 
     }
 
+    /**
+     * 计算距离现在时间
+     * @return
+     */
+    public static Long getBetweenTimes(String time) {
+        long betweenDays = 0;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date plansdate = sdf.parse(time);//预计完成时间
+
+            Calendar calendar = new GregorianCalendar();
+            calendar.setTime(plansdate);
+            //当前时间
+            Date smdate = new Date();
+
+            smdate = sdf.parse(sdf.format(smdate));
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(smdate);
+            long time1 = cal.getTimeInMillis();
+            cal.setTime(calendar.getTime());
+            long time2 = cal.getTimeInMillis();
+            betweenDays = ((time2 - time1) / (1000 * 3600 * 24));//天数
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return betweenDays;
+    }
 }

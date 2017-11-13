6f70636c6498e657b0a24d8d0c960c6c66523421
王东 <>
2017-11-13 06:24:48 +0000

    更新于 Role.java

--- src/main/java/com/marketing/system/entity/Role.java
+++ src/main/java/com/marketing/system/entity/Role.java
@@ -13,6 +13,16 @@ public class Role {
 
     private Integer SystemId;
 
+    private String remark;
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
     public Integer getIndex() {
         return index;
     }

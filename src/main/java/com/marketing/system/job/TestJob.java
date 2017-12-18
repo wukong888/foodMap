7e4d3fae7dd3a4414d17e018e7a37de7b153d1e4
weifei <912118614@qq.com>
2017-12-18 18:01:16 +0800

    修改信息推送

--- src/main/java/com/marketing/system/job/TestJob.java
+++ src/main/java/com/marketing/system/job/TestJob.java
@@ -375,7 +375,7 @@ public class TestJob extends BatchProperties.Job {
     }*/
 
     //下午4点定时提醒更新子任务开发日志
-    @Scheduled(cron="0 45 10 * * ?")
+    @Scheduled(cron="0 05 18 * * ?")
     public ApiResult<List<Map>> subTaskWXPush() {
     System.out.println("=============");
         //获取当前时间
@@ -434,7 +434,7 @@ public class TestJob extends BatchProperties.Job {
     }
 
     //下午4点半判断是否更新日志，并推送
-    @Scheduled(cron="0 47 10 * * ?")
+    @Scheduled(cron="0 06 18 * * ?")
     public ApiResult<List<Map>> subTaskWXPush1() {
         System.out.println("-=-=-=-=-=-=-");
         //获取当前时间
@@ -534,7 +534,7 @@ public class TestJob extends BatchProperties.Job {
     }
 
     //下午5点判断是否更新日志，并推送
-    @Scheduled(cron="0 49 10 * * ?")
+    @Scheduled(cron="0 08 18 * * ?")
     public ApiResult<List<Map>> subTaskWXPush2() {
 
         //获取当前时间
@@ -613,7 +613,7 @@ public class TestJob extends BatchProperties.Job {
     }
 
     //项目实施进度通报
-    @Scheduled(cron="0 00 11 * * ?")
+    @Scheduled(cron="0 09 18 * * ?")
     public ApiResult<List<Map>> progresseport() {
 
         System.out.println("===---===---==");
@@ -722,7 +722,7 @@ public class TestJob extends BatchProperties.Job {
     }
 
     //未实施更新通报
-    @Scheduled(cron="0 00 11 * * ?")
+    @Scheduled(cron="0 09 18 * * ?")
     public ApiResult<List<Map>> noPutCountReport() {
 
         String todayDate = DateUtil.getYMDDate();
